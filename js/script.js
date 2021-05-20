8669c8464ad0e4e924d8cafb82e760c621964081
Heidi Fryzell <heidifryzell@MacBook-Air-2.local>
2021-05-20 08:24:10 -0400

    letter placeholders and console logging entered letter guess

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//The unordered list where the player's guessed letters will appear
+const guessedLetters = document.querySelector(".guessed-letters");
+//The button with the text "Guess!" in it
+const guessButton = document.querySelector(".guess");
+//The text input where the player will guess a letter
+const letterInput = document.querySelector(".letter");
+//The empty paragraph where the word in progress will appear
+const wordInProgress = document.querySelector(".word-in-progress");
+//The paragraph where the remaining guesses will display
+const remainingGuessesParagraph = document.querySelector(".remaining")
+//The span inside the paragraph where the remaining guesses will display
+const remainingGuessesSpan = document.querySelector(".remaining span");
+//The empty paragraph where messages will appear when the player guesses a letter
+const messageToPlayer = document.querySelector(".message");
+//The hidden button that will appear prompting the player to play again
+const playAgainButton = document.querySelector("play-again");
+
+//Starting word to test out the game
+const word = "magnolia";
+
+const createLetterPlaceholders = function (word) {
+    const wordArray = [];
+    for (let letter of word){
+        wordArray.push(`●`);
+    }
+    wordInProgress.innerText = wordArray.join("");
+
+};
+
+createLetterPlaceholders(word);
+
+guessButton.addEventListener("click", function (e){
+    e.preventDefault();
+    const inputValue = letterInput.value;
+    console.log(inputValue);
+});
+
+
+
